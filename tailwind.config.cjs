685ce0ecb3b045caf2eb33bef0369ae821ee7215
AlejandroDalzotto <aledalzotto15@gmail.com>
2023-07-19 19:05:11 -0300

    Change main screen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Vercetti Regular', 'sans-serif']
+		},
 		extend: {
 			fontFamily: {
 				marck: ['Marck Script', 'sans-serif']
 			}
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-animated')
+	],
 }
