a91580823e5a88cb7902421bf2542e1c93ab1419
AlejandroDalzotto <aledalzotto15@gmail.com>
2023-07-19 22:01:32 -0300

    Add view-transitions API

---
+++ src/data/resource-items.ts
@@ -0,0 +1,43 @@
+type ResourceItem = {
+  id: string;
+  name: string;
+};
+
+export const resources: ResourceItem[] = [
+  {
+    id: "libraries",
+    name: "Librerías",
+  },
+  {
+    id: "icons",
+    name: "Iconos",
+  },
+  {
+    id: "tools",
+    name: "Herramientas",
+  },
+  {
+    id: "colors",
+    name: "Colores",
+  },
+  {
+    id: "gradients",
+    name: "Gradientes",
+  },
+  {
+    id: "typography",
+    name: "Tipografias",
+  },
+  {
+    id: "illustrations",
+    name: "Ilustraciones",
+  },
+  {
+    id: "images",
+    name: "Imagenes",
+  },
+  {
+    id: "draw",
+    name: "Diagramar",
+  },
+];
